7c42ebb6a30ecf256b3f4c56e5df0c265e9d33b8
karl.sjostrand@exini.com <karl.sjostrand@exini.com>
2023-09-13 21:25:43 -0400

    Split library into two projects, data and streams. Refactored ByteString -> Array[Byte] in the data project to remove dep on Akka.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,4 +1,3 @@
-import sbt.Keys.resolvers
 import sbt._
 
 object Dependencies {
@@ -9,28 +8,29 @@ object Dependencies {
 
   private lazy val lang: Seq[ModuleID] =
     Seq(
-      "org.scala-lang.modules" %% "scala-xml"           % "2.1.0",
-      "com.beachape"           %% "enumeratum"          % "1.7.2",
+      "org.scala-lang.modules" %% "scala-xml"  % "2.1.0",
+      "com.beachape"           %% "enumeratum" % "1.7.2"
+    )
+
+  private lazy val logging: Seq[ModuleID] =
+    Seq(
+      "org.slf4j" % "slf4j-simple" % "2.0.7"
     )
 
   private lazy val akka: Seq[ModuleID] =
     Seq(
-    "com.typesafe.akka"      %% "akka-stream"         % akkaVersion,
-    "com.typesafe.akka"      %% "akka-slf4j"          % akkaVersion,
-  )
-
-  private lazy val logging: Seq[ModuleID] =
-    Seq(
-      "org.slf4j"               % "slf4j-simple"        % "2.0.7",
+      "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+      "com.typesafe.akka" %% "akka-slf4j"  % akkaVersion
     )
 
   private lazy val test: Seq[ModuleID] =
     Seq(
-      "org.scalatest"          %% "scalatest"           % "3.2.15"     % "test",
-      "com.typesafe.akka"      %% "akka-stream-testkit" % akkaVersion % "test",
+      "org.scalatest"     %% "scalatest"           % "3.2.15"    % "test",
+      "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion % "test"
     )
 
-  lazy val all: Seq[ModuleID] = lang ++ akka ++ logging ++ test
+  lazy val data: Seq[ModuleID]    = lang ++ logging ++ test
+  lazy val streams: Seq[ModuleID] = data ++ akka
 
   lazy val resolvers: Seq[MavenRepository] = Seq(typesafeReleases)
 }
